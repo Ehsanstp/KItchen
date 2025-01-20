f87f779d6aa76159132a26463a03652576954a3a
Ehsanstp <unknowns.cov@.com>
2025-01-20 15:03:47 +0600

    story 2

--- components/Appetizers.tsx
+++ components/Appetizers.tsx
@@ -35,7 +35,7 @@ const Experience = () => {
     {
       title: "Rubber Sheet Geometry",
       description:
-        "What if we can’t see ghosts because they’re from different worlds? Ever wondered about their shapes? Or what they might look like? Add a spoonful of braincells and that's what my intrusive thoughts might look. I would pretend it's a way of asking whether we can know anything about the shapes of higher-dimensional objects (yes, considering ghosts as one). Even though they’re invisible. Now, imagine this strange scenario: Tom, living in a 2D world, can’t see me as I'm 3D, but I can still see him teasing Jerry... duh! But here's the kicker—Tom can only see Jerry as a tiny line zipping across in front of him, not the full cute Jerry visible to us...",
+        "What if we can’t see ghosts because they’re from different worlds? Ever wondered about their shapes? Or what they might look like? Add a spoonful of braincells and that's what my shower thoughts might look. I would pretend it's a way of asking whether we can know anything about the shapes of higher-dimensional objects (yes, considering ghosts as one). Even though they’re invisible. Now, imagine this strange scenario: Tom, living in a 2D world, can’t see me as I'm 3D, but I can still see him teasing Jerry... duh! But here's the kicker—Tom can only see Jerry as a tiny line zipping across in front of him, not the full cute Jerry visible to us...",
       content: (
         <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-slate-900 font-bold">
           Article on Topology
@@ -45,7 +45,7 @@ const Experience = () => {
     {
       title: "The Drowning Light",
       description:
-        "You watch them— the waves— dancing beautifully, up and down, up then down. At first, it seems graceful, like a performance. But then, it hits you, it's not dancing. It's struggling. The waves try to rise, only to fall again, defeated by the weight of the sea. I lower my hands and wonder, why? Why is it that even the water can't find a way to simply stop? I laugh rembering that high-school slogan: Water is life...",
+        "You watch them— the waves— dancing beautifully, up and down, up then down. At first, it seems graceful, like a performance. But then, it hits you; it's not dancing. It's struggling. The waves try to rise, only to fall again, defeated by the weight of the sea. I lower my hands and wonder, why? Why is it that even the water can't find a way to simply stop? I laugh rembering that high-school slogan: Water is life...",
       content: (
         <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--pink-500))] flex items-center justify-center text-black font-bold">
           The Story of an eyewitness
