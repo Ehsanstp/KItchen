1e1c058484488fcd7be1f07e9bb2ec0de95c8f3e
Ehsanstp <unknowns.cov@.com>
2025-01-20 12:48:01 +0600

    ./ src changes

--- data/index.ts
+++ data/index.ts
@@ -49,35 +49,35 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    src: '/Spikey.jpg',
+    src: './Spikey.jpg',
     title: "Procedurally Generated Planet",
     category: "Built a planet, but with spikes... yikes!",
     content: "Planets are fun. Seriously, they’re some of the coolest things in the Universe — and in video games. Big shoutout to games like Outer Wilds, No Man's Sky, and tools like SpaceEngine and Universe Sandbox, which let you appreciate the intricacies of planetary design. So, I attempted, at first as a rookie, to generate a ball with a height-map. Next, I tried to generate a planet where you can adjust the radius, color, surface texture and noise. This noise handling is what makes the planet spikey. These are scientifically not possible but wouldn't our universe be super cool with them? Anyways, currently I'm trying to put my planets in an orbit and hope they don’t just float aimlessly around the void. NASA's previous years Space Apps Challenge has great resources on the Kepler's Laws and generation of orbits. Feel free to check those out if you wanna cook your very own playthings. Also a big thank you to SebLague Tutorials. Great teacher, inspiring coder, hats off."
   },
   {
     id: 2,
-    src: '/Artemis.jpg',
+    src: './Artemis.jpg',
     title: "Artemis Galactic Games",
     category: "NASA Space Apps Challenge",
     content: 'Joint Collaboration on a Challenge to create something fun for the People of Space. There are multiple segments which includes: games, letters, and a Hebron Match'
   },
   {
     id: 3,
-    src: '/logo.jpg',
+    src: './logo.jpg',
     title: "LOGOs & Posters ",
     category: "Human generated art over AI any day",
     content: 'Been designing for 6+ years. Was a part of various Clubs as an Illustrator and created posters and logos. Few are just personal projects. And lastly, one art here is just burnt, as I cooked it when I started using illustrators for the first time, so... kindly ignore the art but do savour the theme.'
   },
   {
     id: 4,
-    src: '/story.jpg',
+    src: './story.jpg',
     title: "Interact With the Game",
     category: "Told ya...burnt games",
     content: 'Ahh, there you go. My Phaser projects are not phasing that much, but I love a game that tells a story.'
   },
   {
     id: 5,
-    src: '/Vidsplainer.jpg',
+    src: './Vidsplainer.jpg',
     title: "Vidsplainers",
     category: "Understand Confusing stuff",
     content: 'This is the work/food that I enjoy/savour every byte/bite (Okay I will STOP!). I love explaining things that I understand. Especially infront of a live audience as it allows me to better grasp what parts are flying above their heads. I love to see the knots untangling in their brain and the Light bulb moment that slowly lits up on their faces. So, I am making videos explaining topics. Although I love/need constant feedbacks; kinda like a trial & error thingy (my explaination runs on feedbacks). But you can still give these Vidsplainers a try.'
@@ -86,14 +86,14 @@ export const projects = [
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: "./git.svg",
   },
   {
     id: 2,
-    img: "/meal.svg",
+    img: "./meal.svg",
   },
   {
     id: 3,
-    img: "/link.svg",
+    img: "./link.svg",
   },
 ];
